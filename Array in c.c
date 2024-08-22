1db8fc38d9dee0bed9011d08c6199808380d162f
Bhuvi21 <bhuvijai2006@gmail.com>
2024-08-22 21:52:25 +0530

    Add files via upload

---
+++ Array in c.c
@@ -0,0 +1,8 @@
+include <stdio.h>
+#include <string.h>
+
+int main() {
+    // declare the array and print 3rd month
+    char month[4][20]={"January","February","March","April"};
+    printf(month[2]);
+}
